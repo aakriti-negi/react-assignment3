a88573790b51c1138b98acfe2fe82b087c78e415
aakriti-negi <aakritinegi40@gmail.com>
2022-05-17 23:09:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Form from './components/Form';
+
 
 function App() {
+
+const [employees, setEmployees] = useState([]);
+
+const addEmployeeHandler = (employee) => {
+  setEmployees([...employees, employee]);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form  addEmployeeHandler={addEmployeeHandler} employees={employees}/>
     </div>
   );
 }
